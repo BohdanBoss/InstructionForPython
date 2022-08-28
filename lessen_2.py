04ed64ac6eaa04b8950d25b5aa1b1816fb7f40a3
bogdansergeevicburduluk <bogdanburduluk@gmail.com>
2022-08-28 22:18:54 +0200

    Add file

---
+++ lessen_2.py
@@ -0,0 +1,34 @@
+# number = 5  # int
+#
+# digit = 4.588689864  # float
+# word = "Result"  # strung
+# boolean = True  # bool
+#
+# str_num = '5'  # string
+#
+# # print(word + str(digit) )
+#
+# print(word + str(number + int(str_num)))
+#
+# del number
+#
+# number = 7
+# print("Result: ", number)
+
+num1 = int(input("Ввидите првое число: "))
+
+num2 = int(input("Ввидите вотрое число: "))
+
+num1 += 5
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 / num2)
+print("Result:", num1 * num2)
+print("Result:", num1 ** num2)  # Возвидение в степень!
+print("Result:", num1 // num2)  # Деление с остатком целого числа!
+
+word = "Hi"
+print(word * 2)
+
+word = 45
